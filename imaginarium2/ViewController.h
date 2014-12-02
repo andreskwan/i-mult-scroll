6cdae149d42f4edc19c483f9c6faec85c9fbc2ed
ANDRES KWAN <andreskwan@gmail.com>
2014-12-02 17:29:55 -0500

    Initial Commit

---
+++ imaginarium2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imaginarium2
+//
+//  Created by Andres Kwan on 12/2/14.
+//  Copyright (c) 2014 Kwan Castle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ imaginarium2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  imaginarium2
+//
+//  Created by Andres Kwan on 12/2/14.
+//  Copyright (c) 2014 Kwan Castle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
